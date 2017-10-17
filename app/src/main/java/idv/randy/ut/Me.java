6d721ec1d15c2325a9ca41bc1dd3416928b94928
Your Name <jack81242002@gmail.com>
2017-10-17 14:50:44 +0800

    finalv1

--- app/src/main/java/idv/randy/ut/Me.java
+++ app/src/main/java/idv/randy/ut/Me.java
@@ -11,7 +11,7 @@ import android.util.Log;
 public class Me extends Application {
     private static Context context;
     private static String ip = "http://10.0.2.2:8081/ba103g1_Android/";
-//        private static String ip = "http://10.120.38.31:8081/ba103g1_Android/";
+//        private static String ip = "http://192.168.196.250:8081/ba103g1_Android/";
     public static final String MembersServlet = ip + "MembersServlet";
     public static final String PetServlet = ip + "PetServlet";
     public static final String PwrServlet = ip + "PwrServlet";
