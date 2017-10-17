bd3394719080abe30d8955304463bb3f4255617f
Your Name <jack81242002@gmail.com>
2017-10-18 01:37:07 +0800

    finalv3

--- app/src/main/java/idv/jack/Common.java
+++ app/src/main/java/idv/jack/Common.java
@@ -8,13 +8,13 @@ import android.widget.Toast;
 
 public class Common { // Android官方模擬器連結本機web server可以直接使用 http://10.0.2.2
 
-//    public final static String URL = "http://10.0.2.2:8081/ba103g1_Android/PetServletAndroid";
-//    public final static String URL1 = "http://10.0.2.2:8081/ba103g1_Android/WebMailServletAndroid";
-//    public final static String URL2 = "http://10.0.2.2:8081/ba103g1_Android/MembersServlet";
+    public final static String URL = "http://10.0.2.2:8081/ba103g1_Android/PetServletAndroid";
+    public final static String URL1 = "http://10.0.2.2:8081/ba103g1_Android/WebMailServletAndroid";
+    public final static String URL2 = "http://10.0.2.2:8081/ba103g1_Android/MembersServlet";
 
-    public final static String URL =  "http://10.120.38.13:8081/ba103g1_Android/PetServletAndroid";
-    public final static String URL1 = "http://10.120.38.13:8081/ba103g1_Android/WebMailServletAndroid";
-    public final static String URL2 = "http://10.120.38.13:8081/ba103g1_Android/MembersServlet";
+//    public final static String URL =  "http://10.120.38.13:8081/ba103g1_Android/PetServletAndroid";
+//    public final static String URL1 = "http://10.120.38.13:8081/ba103g1_Android/WebMailServletAndroid";
+//    public final static String URL2 = "http://10.120.38.13:8081/ba103g1_Android/MembersServlet";
 
     public static boolean networkConnected(Activity activity) {
         ConnectivityManager conManager =
